4cc9e71f0ddd4f30a12c0a6839cabf3c66c01d3d
mann <mannfuri@gmail.com>
2022-01-07 22:55:44 +0900

    基础页面

---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.default')
+@section('title,首页')
+@section('content')
+    <H1>首页</H1>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'StaticPagesController@home')->name('home');
+Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::get('/about', 'StaticPagesController@about')->name('about');
+
