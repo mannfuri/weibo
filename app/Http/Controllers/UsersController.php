bcbe5b2f7b125b24991b33de0392fb7fc1923920
mann <mannfuri@gmail.com>
2022-01-09 03:38:23 +0900

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -37,4 +37,27 @@ class UsersController extends Controller
         Auth::login($user);
         return redirect()->route('users.show',$user);
     }
+
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request,[
+            'name'=> 'required|max:50',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success', '更新信息成功');
+        return redirect()->route('users.show',$user);
+    }
 }
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Clockwork\Support\Laravel\ClockworkMiddleware::class,
-        
+
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
         \Fruitcake\Cors\HandleCors::class,
