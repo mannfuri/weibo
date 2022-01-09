ce4177030b3aae94a4abc3ab5473c5ec0926d561
mann <mannfuri@gmail.com>
2022-01-10 02:49:04 +0900

    重置密码

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Mail;
@@ -48,15 +49,56 @@ class PasswordController extends Controller
         return redirect()->back();
     }
 
-    public function showResetForm()
+    public function showResetForm(Request $request)
         //显示更新密码表单,内置token
     {
-
+        $token = $request->route()->parameter('token');
+        return view('auth.password.reset', compact('token'));
     }
 
-    public function reset()
+    public function reset(Request $request)
         //对提交的token和email进行验证,验证成功更新密码
     {
+        $this->validate($request, [
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        $email = $request->email;
+        $token = $request->token;
+
+        $expires = 6000*10;
+
+        $user = User::where('email',$email)->first();
+
+        if(is_null($user)){
+            session()->flash('danger', '该邮箱没有注册');
+            return redirect()->back();
+        };
+
+        $record=(array) DB::table('password_resets')->where('email',$email)->first();
+        if($record){
+            if(Carbon::parse($record['created_at'])->addSecond($expires)->isPast()){
+                session()->flash('danger', '连接有效期已超时，请重新申请重设密码操作');
+                return redirect()->back();
+            }
+
+            if( ! Hash::check($token,$record['token'])){
+                session()->flash('danger', '令牌错误');
+                return redirect()->back();
+            }
+
+            $user ->update(['password'=> bcrypt($request->password)]);
+            session()->flash('success', '密码重置成功，已帮您自动登录');
+            
+            Auth::attempt(['email'=>$request->email,'password'=>$request->password]);
+            return redirect()->route('users.show', Auth::user());
+        }
+
+        // 6. 记录不存在
+        session()->flash('danger', '未找到重置记录');
+        return redirect()->back();
 
     }
 
