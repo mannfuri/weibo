a80dd12d6920421bf5a8d78a5390381059b8c344
mann <mannfuri@gmail.com>
2022-01-11 03:22:12 +0900

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -70,4 +70,37 @@ class User extends Authenticatable
             ->with('user')
             ->orderBy('created_at', 'desc');
     }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class,'followers', 'user_id', 'follower_id');
+    }
+
+    public function followings()
+    {
+        return $this->belongsToMany(User::class,'followers', 'follower_id', 'user_id');
+    }
+
+    public function follow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact(user_ids);
+        }
+        //sync方法需要接受一个数组,所有需要上面这个操作把collection转成数组
+        $this->followings()->sync($user_ids, false);
+    }
+
+    public function unfollow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact(user_ids);
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    public function isfollowing($user_id)
+    {
+        return $this->followings()->contains($user_id)
+    }
+
 }
